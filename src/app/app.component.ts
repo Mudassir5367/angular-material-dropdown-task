363315d5bb50beb13cbee5954841ae88d2a19faa
Mudassir5367 <mudassirhussain32303@gmail.com>
2024-04-23 23:30:02 +0500

    material dropdown

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'material-dropdwon-task';
+
+  isDropdownOpen: boolean = false;
+  selectedOption: string = '';
+
+  toggleDropdown(): void {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+
+  onOptionSelect(option: string): void {
+    this.selectedOption = option;
+    this.isDropdownOpen = false; // Close the dropdown after selection
+    console.log('Selected Option:', option);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatSelectModule } from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatSelectModule,
+  BrowserAnimationsModule,
+  FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
